081e92680638ce684a2a82e3ad1da7d5363e595f
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-06-11 23:56:45 -0500

    Time: 5 ms (91.27%), Space: 93.1 MB (28.79%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
@@ -1,13 +1,33 @@
 class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
-        //TimeComplexity : N*LOGN
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int j = nums1.length-1;
-        int sum=0;
+        //TimeComplexity : (n)
+        int a[] = new int[101];
+        int b[] = new int[101];
         for(int i=0;i<nums1.length;i++){
-            sum+= nums1[i]*nums2[j];
-            j--;
+            a[nums1[i]]++;
+            b[nums2[i]]++;
+        }
+        int i=0,j=100;
+        int sum=0;
+        while(i<101 && j>=0){
+            if(a[i] == 0&&b[j]==0) {
+                i++;j--;
+            }
+            else if(a[i]==0) {
+                i++;
+            }
+            else if(b[j]==0) {
+                j--;
+            }
+            else{
+                sum+=i*j;
+                a[i]-=1;
+                b[j]-=1;
+                    //i++;
+                    //j--;
+                
+            }
+                
         }
         return sum;
     }
