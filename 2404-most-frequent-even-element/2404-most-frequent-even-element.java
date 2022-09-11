6dafdeebe9f480becd3ff52eca7e0fa8205622e5
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-09-11 14:42:03 -0500

    Time: 56 ms (27.27%), Space: 62.3 MB (11.11%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        int min = 0;
+        int ans = -1;
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2 == 0){
+                hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+                if(hm.get(nums[i]) > min){
+                    ans = nums[i];
+                    min = hm.get(nums[i]);
+                }else if(hm.get(nums[i]) == min){
+                    ans = Math.min(ans,nums[i]);
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
