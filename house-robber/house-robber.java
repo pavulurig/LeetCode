f58cd51e14f92ed95abd05b4d9f44d44c1e166a9
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-09-16 12:58:20 -0500

    Time: 0 ms, Memory: 41.3 MB - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -2,9 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         //Top Down
        Integer dp[] = new Integer[nums.length+1];
-        return topdown(nums,nums.length-1,dp);
+        return bottomup(nums,dp);
+        //return topdown(nums,nums.length-1,dp);
     }
-    
+    int bottomup(int[] nums,Integer[] dp){
+        dp[0]=nums[0];
+        if(nums.length == 1) return dp[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+        
+    }
     int topdown(int[] nums,int i,Integer[] dp){
         if(i == 0) return nums[0];
         if(i == 1) return Math.max(nums[0],nums[1]);
