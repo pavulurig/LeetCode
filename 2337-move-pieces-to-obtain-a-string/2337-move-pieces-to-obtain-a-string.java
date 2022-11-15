bd8e2a6095de0c844c2ce077c660615815378170
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-11-15 00:23:16 -0600

    Time: 23 ms (84.07%), Space: 44 MB (93.41%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -1,54 +1,23 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        class Node{
-            char ch;
-            int index;
-        }
-        
-        List<Node>startList = new ArrayList<>();
-        List<Node>endList = new ArrayList<>();
-        for(int i=0;i<start.length();i++){
-            if(start.charAt(i)!='_'){
-              Node n = new Node();
-                n.ch= start.charAt(i);
-                n.index=i;
-                startList.add(n);  
+        int i=0,j=0;
+        while(i<start.length() || j<target.length()){
+            while(i<start.length()  && start.charAt(i)=='_'){
+                i++;
             }
-            
-        }
-        
-        for(int i=0;i<target.length();i++){
-            if(target.charAt(i)!='_'){
-               Node n = new Node();
-                n.ch= target.charAt(i);
-                n.index=i;
-                endList.add(n); 
+            while(j<target.length() && target.charAt(j)=='_'){
+                j++;
             }
-            
-        }
-        
-        if(startList.size()!=endList.size()){
-            return false;
-        }
-        
-        int i =0,j=0;
-        while(i<startList.size() && j<endList.size()){
-            if(startList.get(i).ch != endList.get(j).ch){
-                return false;
-            }else{
-                if(startList.get(i).ch=='R'){
-                    if(startList.get(i).index>endList.get(j).index){
-                        return false;
-                    }
-                }else{
-                    if(startList.get(i).index<endList.get(j).index){
-                        return false;
-                    }
-                }
-            }
+            if( (i == start.length()) || (j == start.length())) break;
+            if(start.charAt(i)!=target.charAt(j)) break;
+            if(start.charAt(i) == 'L' && j>i) break;
+            if(start.charAt(i) == 'R' && i>j) break;
             i++;
             j++;
         }
-        return true;
+        System.out.println(i);
+         System.out.println(j);
+        
+        return i==start.length() && j== target.length();
     }
 }
