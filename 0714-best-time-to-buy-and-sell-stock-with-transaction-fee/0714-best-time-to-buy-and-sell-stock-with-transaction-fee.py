a4ab9b01511167c1ffe3d3232b3c0e0e128b4ec7
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-08-29 18:04:50 -0500

    Time: 1037 ms (22.88%), Space: 109.5 MB (15.47%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @lru_cache(maxsize = 10000)
+        @lru_cache(maxsize = 1000)
         def f(i,j):
             if i >= len(prices):
                 return 0
