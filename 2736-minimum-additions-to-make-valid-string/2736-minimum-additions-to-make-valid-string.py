44230c85d48ff7568173da600434840c67638ef6
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-08-30 16:06:51 -0500

    Time: 51 ms (42.25%), Space: 16.5 MB (9.12%) - LeetHub

---
+++ 2736-minimum-additions-to-make-valid-string/2736-minimum-additions-to-make-valid-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        possible = {'a':2,'b':2,'c':2,"ab":1,'bc':1,"ac":1,"abc":0}
+        find = ""
+        ans = 0
+        for w in word:
+            if find == "":
+                find+=w
+            elif find[-1] < w:
+                find+=w
+            else:
+                print(possible[find],find)
+                ans+=possible[find]
+                find= ""
+                find+=w
+            #print(find)
+        if find!="":
+            ans+=possible[find]
+
+        return ans
