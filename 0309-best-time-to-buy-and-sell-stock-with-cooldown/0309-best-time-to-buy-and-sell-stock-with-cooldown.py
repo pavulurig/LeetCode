0d608b82d3dd8c9cdf06fe82075b4778a1f6ca45
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-09-04 23:33:05 -0500

    Time: 44 ms (88.52%), Space: 21.1 MB (32.15%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache(maxsize=10000)
-        def f(i,isllowedtobuy):
+        @lru_cache(maxsize=5000)
+        def f(i,allowedtobuy):
             if i >= len(prices):
                 return 0
-            ans =0
-            if isllowedtobuy == 1:
-                ans+= max(-prices[i]+f(i+1,0),f(i+1,1))
+            if allowedtobuy == 1:
+                return max(-prices[i]+f(i+1,0),f(i+1,1))
             else:
-                ans+= max(prices[i]+f(i+2,1),f(i+1,0))
-            return ans
+                return max(prices[i]+f(i+2,1),f(i+1,0))
+        
         return f(0,1)
+        
