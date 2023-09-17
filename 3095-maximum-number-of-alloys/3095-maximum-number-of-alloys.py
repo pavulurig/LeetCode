1a6cef6aac3a8e2ea2353257253c008f46ce5244
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-09-17 11:37:03 -0500

    Time: 785 ms (25%), Space: 16.7 MB (100%) - LeetHub

--- 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.py
+++ 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.py
@@ -14,12 +14,4 @@ class Solution:
                     left = mid+1
                 else:
                     right = mid-1
-                    
-            
         return ans
-            
-
-
-                
-            
-        
