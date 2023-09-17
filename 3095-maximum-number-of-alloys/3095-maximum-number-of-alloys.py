7f4dd8e099fef87db9c793e387d000fd41c79db2
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-09-17 11:33:08 -0500

    Time: 815 ms (25%), Space: 16.8 MB (50%) - LeetHub

---
+++ 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxNumberOfAlloys(self, n: int, k: int, budget: int, composition: List[List[int]], stock: List[int], cost: List[int]) -> int:
+        ans = 0
+        for i in range(k):
+            left,right = 0,10**9
+            while left <= right:
+                mid = (left+right)//2
+                total_budget = 0
+                for j in range(n):
+                    total_budget+=(max(0,(mid*composition[i][j])-stock[j])*cost[j])
+                
+                if total_budget <= budget:
+                    ans = max(ans,mid)
+                    left = mid+1
+                else:
+                    right = mid-1
+                    
+            
+        return ans
+            
+
+
+                
+            
+        
