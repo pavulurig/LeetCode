5c6b5452f78edcf4455d46066e1e8cc5010a68e5
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-09-17 23:28:48 -0500

    Time: 802 ms (42.6%), Space: 16.3 MB (91.23%) - LeetHub

---
+++ 3092-minimum-moves-to-spread-stones-over-grid/3092-minimum-moves-to-spread-stones-over-grid.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumMoves(self, grid: List[List[int]]) -> int:
+        zero = []
+        more_than_one = []
+        moves = inf
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j]==0:
+                    zero.append([i,j])
+                elif grid[i][j]>1:
+                    for _ in range(grid[i][j]-1):
+                        more_than_one.append([i,j])
+        
+        for perm in permutations(more_than_one, len(zero)):
+            #print(perm)
+            total_distance = 0
+            for i in range(len(zero)):
+                total_distance += abs(zero[i][0]-perm[i][0])+abs(zero[i][1]-perm[i][1])
+            moves = min(moves, total_distance)
+
+        return moves
+
+        
