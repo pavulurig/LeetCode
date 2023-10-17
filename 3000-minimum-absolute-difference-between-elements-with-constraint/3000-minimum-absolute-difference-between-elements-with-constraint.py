7de3485b2888c492312338931b2b67e69e7c9cac
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2023-10-17 18:25:03 -0500

    Time: 1627 ms (19.54%), Space: 36.6 MB (17.8%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.py
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.py
@@ -2,16 +2,18 @@ from sortedcontainers import SortedList
 class Solution:
     def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
         sorted_array = SortedList(key = lambda k : k[0])
+        '''
+            Time : O((n-x)*logn(n))
+        '''
         min_ans = abs(nums[0] - nums[-1])
         for i in range(x,len(nums)):
             insertt = (nums[i-x],i-x)
-            sorted_array.add(insertt)
-            pos = sorted_array.bisect_left((nums[i],i))
+            sorted_array.add(insertt) # log(n)
+            pos = sorted_array.bisect_left((nums[i],i)) # log(n)
             if len(sorted_array) >= pos+1:
                 min_ans = min(min_ans,abs(sorted_array[pos][0]-nums[i]))
             if len(sorted_array) >= pos+2:
                 min_ans = min(min_ans,abs(sorted_array[pos+1][0]-nums[i]))
-            
             min_ans = min(min_ans,abs(sorted_array[pos-1][0]-nums[i]))
                
         return min_ans
