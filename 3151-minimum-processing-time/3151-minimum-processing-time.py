2f8f58ece5f06b16c52a4f6077f4f150c857b717
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2023-10-18 18:24:16 -0500

    Time: 596 ms (92.06%), Space: 32.7 MB (61.73%) - LeetHub

---
+++ 3151-minimum-processing-time/3151-minimum-processing-time.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        #lct: sorting;
+        #lcd: M_1
+        processorTime.sort()
+        tasks.sort()
+        j = 0
+        ans = 0
+        for i in range(len(tasks)-1,-1,-4):
+            ans = max(ans,tasks[i]+processorTime[j])
+            j+=1
+            
+        return ans
+            
