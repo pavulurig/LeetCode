45d4f60294ce73ee0ec0ca70b52907296a10d2b7
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2023-10-18 18:00:13 -0500

    Time: 5 ms (47.46%), Space: 56.1 MB (5.47%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1;
+        int ans = 0;
+        // lct:twopointer;
+        while(l<r){
+            int area = Math.min(height[l],height[r])*(r-l);
+            ans =  Math.max(ans,area);
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return ans;
+    }
+}
