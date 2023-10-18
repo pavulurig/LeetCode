6e5681132264c76b7d29d7e473ec1baada6557aa
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2023-10-18 18:06:06 -0500

    Time: 5 ms (47.46%), Space: 55.6 MB (34.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,6 +3,7 @@ class Solution {
         int l=0,r=height.length-1;
         int ans = 0;
         // lct:twopointer;
+        // lcd:M_1;
         while(l<r){
             int area = Math.min(height[l],height[r])*(r-l);
             ans =  Math.max(ans,area);
