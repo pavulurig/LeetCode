53539d8d1feca802e49d502611b0b34ee39a65bf
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2023-10-18 15:26:06 -0500

    Time: 1151 ms (92.03%), Space: 31.2 MB (91.32%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.py
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.py
@@ -1,12 +1,10 @@
-'''
-     Binary Search & Ordered List
-'''
 class Solution:
     def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
         sorted_array = []
+        #lct:orderedset,binarysearch;
         min_ans = abs(nums[0] - nums[-1])
         for i in range(x,len(nums)):
-            insort(sorted_array,nums[i-x])
+            insort(sorted_array,nums[i-x]) 
             pos = bisect_left(sorted_array,nums[i])
             if pos < len(sorted_array):
                 min_ans = min(min_ans,abs(sorted_array[pos]-nums[i]))
