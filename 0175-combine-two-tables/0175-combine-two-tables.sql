38166403a81530521d9314189060b4d22924e6b4
Gopikrishna Pavuluri <gxp5903@mavs.uta.edu>
2024-02-08 13:07:07 -0600

    Time: 893 ms (27.46%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-
+# lct: sql,join;
+# lcd: E1;
 select p.firstName , p.lastName , a.city ,a.state from Person as p
 left join Address as a
 ON p.personId = a.personId;
